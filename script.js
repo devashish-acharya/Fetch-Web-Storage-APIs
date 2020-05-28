7a01f5d5a6cca444eeb17f00793b4e143c06ade6
devashish-acharya <thedev96@gmail.com>
2020-05-28 14:08:26 +0100

    test

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall', {
   } else {console.log("Oh no!")}
 })
 
+fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall', {
+  method: 'POST',
+  body: JSON.stringify({message: 'fiddle'})
+}).then(function (response) {
+  if (response.status == 200) {
+    console.log("Ok!")
+  } else {console.log("Oh no!")}
+})
+
 fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(data => console.log(data))
 
 if (localStorage.getItem('test') == "key") {
